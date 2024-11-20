5f26bd0691ef55b9d9e5f7dcb6a21ed06f5c198b
SeanThakur <seanthakur20@gmail.com>
2024-11-21 01:29:11 +0530

    matrix init

--- algorithms/matrix.js
+++ algorithms/matrix.js
@@ -1,1 +1,5 @@
 // array multi-dimensional array operations
+// Operation
+// Matrix to zero, spiral clockwise and anti clockwise, rotate 90 clockwise and anti-clockwise, matrix symmetric, rotate matrix to right
+// matrix flip bit, empty cell in matrix, search in matrix, find peak element, Search in a row and column wise sorted matrix and
+// Find the row with maximum number of 1's
