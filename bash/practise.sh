ee52dc350edeb978f79da16070770dcca2ead6d0
SeanThakur <vikash_thakur@amperehourenergy.com>
2025-07-18 07:48:51 +0530

    wip(bash/practise): added redirection process to it

--- bash/practise.sh
+++ bash/practise.sh
@@ -3,6 +3,9 @@
 time=$(date +%H:%m:%S)
 x=2
 y=5
+output="$(dirname "$0")/practise_output.txt"
 echo "aur lavdii $USER tera ye time ho raha hai $time"
 echo "2 and 5 plus karke $((x+y)) itna hota hai"
+echo "this is brace and arthematic expansion example:" {1..3} "and $((3+4))"
+echo $HOSTNAME > $output
 exit 0;
